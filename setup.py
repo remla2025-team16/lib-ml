bb601e006039a5860583eae30e9e160af5185fe7
Anyan Huang <a.huang-3@student.tudelft.nl>
2025-05-05 19:30:34 +0200

    fix: add package setup files

--- setup.py
+++ setup.py
@@ -1,17 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="libml",
-    version="0.1.0",  # Will be overridden by GitHub tag
-    description="Reusable preprocessing library for sentiment analysis",
-    author="Gyum Cho",
-    packages=find_packages(),
-    install_requires=[
-        "scikit-learn"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
-    ],
-    python_requires='>=3.6',
-)
+from setuptools import setup
+setup()
